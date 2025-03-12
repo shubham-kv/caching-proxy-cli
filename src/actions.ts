0bbb8cf30f1302ee2f1bcb3e9e48b9f67bb0eb08
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-03-12 16:06:44 +0530

    feat: file caching for 'text/*', 'image/*' & 'audio/*' mime types

--- src/actions.ts
+++ src/actions.ts
@@ -1,29 +1,101 @@
+import path from "path";
+import { createWriteStream, existsSync } from "fs";
+import { mkdir } from "fs/promises";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
 import { Command } from "@commander-js/extra-typings";
+
 import { app } from "./app";
-import { cache, cacheMiddleware } from "./middlewares";
+import { cache, cacheMiddleware, fileCacheMiddleware } from "./middlewares";
 
 type StartCommand = Command<[string], { port: number }, {}>;
 
 export function startServer(this: StartCommand) {
   const [origin] = this.processedArgs;
-  const options = this.opts();
+  const { port } = this.opts();
 
-  app.get("*", cacheMiddleware, async (req, res) => {
-    const path = req.path;
-    const originResponse = await fetch(origin + path);
-    const jsonData = await originResponse.json();
+  const axiosInstance = axios.create({ baseURL: origin });
 
-    cache[req.path] = jsonData;
+  app.get("*", cacheMiddleware, fileCacheMiddleware, async (req, res) => {
+    // ** 1. Get upstream response
+    let upstreamResponse: AxiosResponse<any, any>;
+    try {
+      upstreamResponse = await axiosInstance.get(req.path, {
+        responseType: "stream",
+      });
+    } catch (e) {
+      console.error("Something went wrong");
+      console.log(e);
 
-    res.setHeader("X-Cache", "MISS");
-    res.json(jsonData);
+      if (e instanceof AxiosError) {
+        if (e.response) {
+          res.setHeader("X-Cache", "MISS");
+          res.status(e.response.status).send("Bad Request");
+          return;
+        } else if (e.request) {
+        } else {
+        }
+      }
+
+      res.status(500).send("Something went wrong");
+      return;
+    }
+
+    // ** 2. Set the response headers
+    const contentType = upstreamResponse.headers["content-type"] as string;
+
+    const headers = new Headers();
+    Object.entries(upstreamResponse.headers).forEach(([header, value]) => {
+      headers.set(header, value);
+    });
+    headers.set("X-Cache", "MISS");
+    res.setHeaders(headers);
+
+    // ** 3. Cache upstream response & send back response to client with X-Cache MISS header
+    const pathHasExtension = path.extname(req.path);
+
+    if (
+      pathHasExtension ||
+      contentType.startsWith("text/") ||
+      contentType.startsWith("image/") ||
+      contentType.startsWith("audio")
+    ) {
+      const cacheFilePath = path.join(__dirname, "../cache", req.path);
+      const parsedPath = path.parse(path.normalize(cacheFilePath));
+
+      if (!existsSync(parsedPath.dir)) {
+        try {
+          await mkdir(parsedPath.dir, { recursive: true });
+        } catch (e) {
+          console.log(`Failed to create cache directory '${parsedPath.dir}'`);
+          console.error(e);
+          res.status(500).send("Something went wrong");
+          return;
+        }
+      }
+
+      const writeStream = createWriteStream(cacheFilePath, "utf-8");
+      upstreamResponse.data.pipe(writeStream);
+
+      writeStream.on("error", (e) => {
+        console.error("Received an error while streaming the response");
+        console.error(e);
+        res.status(500).send("Something went wrong");
+      });
+
+      writeStream.on("finish", () => {
+        res.status(upstreamResponse.status).sendFile(cacheFilePath);
+      });
+    } else {
+      throw new Error('Yet to implement')
+    }
   });
 
-  app.listen(options.port, (e) => {
+  app.listen(port, (e) => {
     if (e) {
       console.error(e);
     } else {
-      console.log(`Caching proxy running on http://localhost:${options.port}`);
+      console.log(`Caching proxy running on http://localhost:${port}`);
     }
   });
 }
