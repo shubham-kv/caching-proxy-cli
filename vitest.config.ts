fe27fffcc311dcdb242111019f2c2ef19b74b39f
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-03-16 15:18:55 +0530

    chore(vitest.config): decrease hook & test timeouts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    hookTimeout: 60 * 1000,
-    testTimeout: 60 * 1000
+    hookTimeout: 30 * 1000,
+    testTimeout: 30 * 1000
   },
 });
