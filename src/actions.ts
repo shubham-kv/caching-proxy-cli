ccd961870a641e355cf88452e625aece8534051a
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-03-17 16:00:34 +0530

    feat: add clear-cache command

--- src/actions.ts
+++ src/actions.ts
@@ -1,4 +1,6 @@
 import { Command } from "@commander-js/extra-typings";
+import { existsSync } from "fs";
+import { rm } from "fs/promises";
 
 import { configureAndStartCacheProxy } from "./proxy";
 import { isIntegerString } from "./utils/string";
@@ -6,7 +8,7 @@ import { cacheDirPath, testCacheDirPath } from "./constants";
 
 type StartCommand = Command<[string], { port: string }, {}>;
 
-export function startServer(this: StartCommand) {
+export function startServer(this: StartCommand): void {
   const [origin] = this.processedArgs;
   const { port } = this.opts();
 
@@ -31,4 +33,20 @@ export function startServer(this: StartCommand) {
     cacheDirectory:
       process.env.NODE_ENV === "test" ? testCacheDirPath : cacheDirPath,
   });
+}
+
+export async function clearCache(this: Command<[], {}, {}>): Promise<void> {
+  const cacheDir =
+    process.env.NODE_ENV === "test" ? testCacheDirPath : cacheDirPath;
+
+  if (existsSync(cacheDir)) {
+    try {
+      await rm(cacheDir, { force: true, recursive: true });
+      console.log('Cleared the cache successfully!');
+    } catch (e) {
+      this.error("Failed to remove the cache directory, try again");
+    }
+  } else {
+    this.error("Invalid cache directory");
+  }
 }
